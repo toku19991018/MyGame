0b22ff1e3b3fdf7754dd2ffec7254cc0cb3f871d
tokunagaryunosuke <patario18@gmail.com>
2019-05-25 15:58:39 +0900

    画像変更

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -101,20 +101,10 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+	Sprite* sprite = Sprite::create("Dorakue.png");
+	this->addChild(sprite);
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
+
     return true;
 }
 
